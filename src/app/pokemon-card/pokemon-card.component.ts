9f1c7455fd8bd56ccc086aaec43b633591798a2d
Luis Felipe <luis.mendes@totvs.com.br>
2022-06-09 17:34:48 -0300

    add/card de styles e list pokemon

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Pokemon } from 'src/_model/Pokemon';
 
 @Component({
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
   styleUrls: ['./pokemon-card.component.sass']
 })
-export class PokemonCardComponent implements OnInit {
+export class PokemonCardComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  @Input('pokemon')
+  public pokemon: Pokemon | undefined;
 }
--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Pokemon } from 'src/_model/Pokemon';
+import { Type } from 'src/_model/Type';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.sass']
 })
-export class PokemonListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class PokemonListComponent {
+  public pokemons: Pokemon[] = [{
+        image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+        number: 1,
+        name: 'Bulbassaur',
+        types: [
+            Type.Grass,
+            Type.Poison
+          ],
+        },
+    ];
 }
+
+
+
